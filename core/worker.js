59478a40c30bf7681884e5fd76a94cf12a7dfa8d
Minwoo Jung <jmwsoft@gmail.com>
2015-10-23 16:40:43 +0900

    Update worker.js

--- core/worker.js
+++ core/worker.js
@@ -1,6 +1,6 @@
 var rabbitHub = require('../lib/hub');
 
-var workerHub = rabbitHub.create( { task: 'worker', channel: 'myChannel' } );
+var workerHub = rabbitHub.create( { task: 'worker', channel: 'QAChannel' } );
 workerHub.on('connection', function(hub) {
 
   hub.on('message', function(msg) {
